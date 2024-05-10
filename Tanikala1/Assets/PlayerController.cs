0812ba81d6d395e1282af57f10bb992cbc4eccc3
ceejayjj21 <charlierudolf44@gmail.com>
2024-05-10 11:04:51 +0800

    Character Movement
    Character Movement yarn

---
+++ Tanikala1/Assets/PlayerController.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float groundDist;
+
+    public LayerMask terrainLayer;
+    public Rigidbody rb;
+    public SpriteRenderer sr;
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Vector3 castPost = transform.position;
+
+        castPost.y += 1;
+        if(Physics.Raycast(castPost, -transform.up, out hit,Mathf.Infinity, terrainLayer)) 
+        { 
+            if(hit.collider != null)
+            {
+                Vector3 movePos = transform.position;
+                movePos.y = hit.point.y + groundDist;
+                transform.position = movePos;
+            }
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        Vector3 movDir = new Vector3(x, 0,y);
+        rb.velocity = movDir * speed;
+
+        if(x != 0 && x < 0)
+        {
+            sr.flipX = true;
+        }
+        else if(x != 0 && x > 0)
+        {
+            sr.flipX = false;
+        }
+        
+    }
+}
